b375cba91e33450d9e0adb66386692289a9743c0
marioyeung <maryeungth@gmail.com>
2016-06-21 16:04:13 -0700

    Set up the skeleton of webpage and routing

--- public/client/index.js
+++ public/client/index.js
@@ -1,6 +1,30 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import d3 from './../../lib/d3'
 import $ from 'jquery';
 import chart from './../assets/statChartBuilder';
+
+// import other components
+import Main from './main';
+import Nav from './nav';
+import Mock from './mock';
+
+// <Route path='/' component={Login}/>
+// <Route path='/login' component={Login}/>
+// <Route component={Main}>
+//   <Route path='create' component={Form}/>
+//   <Route path='dashboard' component={Feed}/>
+//   <Route path='post/:arcId' component={FacebookPost}/>
+// </Route>
+render((
+  <Router history={browserHistory}>
+    <Route component={Main}>
+      <Route path='/' component={Mock} /> 
+    </Route>
+  </Router>
+), document.getElementById('container'));
+
 
 // mock data
 // arr[0]: average fork # (y)
@@ -36,24 +60,24 @@ const mockForkData = [
   [1300, 18000]
 ];
 
-chart('d3Graph', 'graph1', mockForkData, 25000, 'Followers', 2000, 'Average Fork');
+// chart('d3Graph', 'graph1', mockForkData, 25000, 'Followers', 2000, 'Average Fork');
 
-chart('d3Graph2', 'graph2', [[1, 2], [100, 200], [1000, 2000], [10000, 20000]], 30000, undefined, 15000);
+// chart('d3Graph2', 'graph2', [[1, 2], [100, 200], [1000, 2000], [10000, 20000]], 30000, undefined, 15000);
 
-$.ajax({
-  method: 'GET',
-  url: 'https://api.github.com/search/users?q=followers:%3E700&sort=followers'
-}).done( (data) => {
-  let topUsers = [];
-  for ( var users of data.items ) {
-    console.log(users.login);
-    $.ajax({
-      method: 'GET',
-      url: `https://api.github.com/users/${users.login}`
-    }).done ( (user) => {
-      topUsers.push(user);
-      console.log(user.followers);
-      console.log(topUsers);
-    })
-  }
-});
+// $.ajax({
+//   method: 'GET',
+//   url: 'https://api.github.com/search/users?q=followers:%3E700&sort=followers'
+// }).done( (data) => {
+//   let topUsers = [];
+//   for ( var users of data.items ) {
+//     console.log(users.login);
+//     $.ajax({
+//       method: 'GET',
+//       url: `https://api.github.com/users/${users.login}`
+//     }).done ( (user) => {
+//       topUsers.push(user);
+//       console.log(user.followers);
+//       console.log(topUsers);
+//     })
+//   }
+// });
