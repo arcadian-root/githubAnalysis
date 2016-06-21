88fac61c383e266ef67e0ae12dcb7089a9e363e7
marioyeung <maryeungth@gmail.com>
2016-06-21 12:08:07 -0700

    Set up basic server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+//TODO: define environment variable
+//TODO: setup authentication if hnecessary
+
+app.use(express.static(__dirname + "/../"));
+
+// middleware
+app.use(bodyParser.json());
+
+// TODO: setup endpoints
+
+// TODO: accept env variable if procided
+app.listen(3000, function(){
+  console.log('Listen to the port 3000');
+});
