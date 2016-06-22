b3a56a3bef04f99373c8ea668621c87f3244614d
marioyeung <maryeungth@gmail.com>
2016-06-22 11:35:33 -0700

    Configure github strategy and authenticate request

--- server/server.js
+++ server/server.js
@@ -2,6 +2,39 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+// authentication
+var session = require('express-session');
+var passport = require('passport');
+// TODO: create and require a config file
+var GITHUB_CLIENT_ID = process.env.BOCKS_CLIENT_ID || config.BOCKS_CLIENT_ID;
+var GITHUB_CLIENT_SECRET = process.env.BOCKS_CLIENT_SECRET || config.BOCKS_CLIENT_SECRET;
+var BOCKS_SECRET = process.env.BOCKS_SESSION_SECRET || config.BOCKS_SESSION_SECRET;
+// Configure Github authentication strategy
+var Strategy = require('passport-github').Strategy;
+passport.use(new Strategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL:
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+// Authenticate requests
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    // TODO: define this url dashboard is created
+    res.redirect('/');
+  });
+
+
 //TODO: define environment variable
 //TODO: setup authentication if hnecessary
 
