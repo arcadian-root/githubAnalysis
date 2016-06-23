64436338c5fde77642968bbf5a2a796367197402
Michael Daly <mjdaly95@gmail.com>
2016-06-23 14:53:57 -0700

    Moved routing to requestHandler and added GETting repos from database

--- server/requestHandler.js
+++ server/requestHandler.js
@@ -1,5 +1,25 @@
 var path = require('path');
+var passport = require('passport');
 
-exports.wildcard = function(req, res) {
-  res.sendFile(path.resolve(__dirname, './../public', 'index.html'));
-}
+var db = require('./database/database.js');
+
+module.exports = function (app, express) {
+  app.get('/api/v1/repos/:name', db.getRepo);
+  // app.get('/api/v1/users/:name', db.getUser);
+
+  app.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './../public', 'index.html'));
+  });
+
+  // Authenticate requests
+  app.get('/auth/github', passport.authenticate('github'));
+
+  app.get('/auth/github/callback', passport.authenticate('github', 
+    { failureRedirect: '/login' }),
+
+    function(req, res) {
+      // Successful authentication, redirect home.
+      // TODO: define this url dashboard is created
+      res.redirect('/sample');
+    });
+};
