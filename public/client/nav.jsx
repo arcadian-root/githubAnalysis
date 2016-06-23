999876ee9088b8a0d2fb3cfe40ea2daf5a8dcf2d
marioyeung <maryeungth@gmail.com>
2016-06-23 14:00:45 -0700

    Add ability to get user information from server

--- public/client/nav.jsx
+++ public/client/nav.jsx
@@ -4,16 +4,38 @@ import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import { NavItem } from 'react-bootstrap';
 import { Link, browserHistory } from 'react-router';
+import $ from 'jquery';
 
-let nav = (props) => (
-  <Navbar>
-    <Nav>
-      <Link to='/'>Home</Link>
-      <Link to='sample'>Sample</Link>
-      <a href='/auth/github/callback'>Login</a>
-      <a href='/logout'>Logout</a>
-    </Nav>
-  </Navbar>
-)
+class Navigator extends React.Component { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: null
+    }
+  }
 
-export default nav;
+  componentDidMount () {
+    $.ajax({
+      method: 'GET',
+      url: '/user/info'
+    }).done( (data) => {
+      console.log(data)
+      this.setState({userInfo: data});
+    });
+  }
+
+  render () {
+   return (
+      <Navbar>
+        <Nav>
+          <Link to='/'>Home</Link>
+          <Link to='sample'>Sample</Link>
+          <a href='/auth/github/callback'>Login</a>
+          <a href='/logout'>Logout</a>
+        </Nav>
+      </Navbar>
+    )
+  }
+}
+
+export default Navigator;
