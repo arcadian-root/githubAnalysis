94110f5e5becc5da11a7c6f90b3e5076637820a0
Michael Daly <mjdaly95@gmail.com>
2016-06-23 17:16:45 -0700

    Moved endpoints to routes.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
+
+require('./routes.js')(app, express);
+
 var bodyParser = require('body-parser');
 var path = require('path');
-var requestHandler = require('./requestHandler');
+
 var session = require('express-session');
 var passport = require('passport');
-var config = require('./../config/config');
+
+var config = require('../config/config');
 var GITHUB_CLIENT_ID = process.env.CLIENT_ID || config.CLIENT_ID;
 var GITHUB_CLIENT_SECRET = process.env.CLIENT_SECRET || config.CLIENT_SECRET;
 var SECRET = process.env.SESSION_SECRET || config.SESSION_SECRET;
@@ -14,7 +18,9 @@ var userInfo = null;
 // authentication
 app.use(passport.initialize());
 app.use(passport.session());
+
 // Configure Github authentication strategy
+
 var Strategy = require('passport-github').Strategy;
 passport.use(new Strategy({
     clientID: GITHUB_CLIENT_ID,
@@ -25,25 +31,6 @@ passport.use(new Strategy({
     return cb(null, profile);
   }
 ));
-// Authenticate requests
-app.get('/auth/github', passport.authenticate('github'));
-
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/' }),
-  // Successful authentication, redirect dashboard.
-  function(req, res) {
-    // Get user data
-    userInfo = req.user;
-    res.redirect('/dashboard');
-  }
-);
-
-app.get('/logout', function(req, res) {
- console.log('in /user/logout');
- userInfo = null;
- req.logout();
- res.redirect('/');
-});
 
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -58,13 +45,6 @@ passport.deserializeUser(function(obj, done) {
 app.use(express.static(__dirname + "/../public/"));
 app.use(bodyParser.json());
 
-// TODO: setup endpointss
-app.get('/user/info', function(req, res) {
-  console.log(userInfo)
-  res.send(userInfo);
-})
-app.get('/sample', requestHandler.wildcard);
-app.get('/dashboard', requestHandler.wildcard);
 
 // TODO: accept env variable if procided
 var PORT = process.env.PORT || 3000;
