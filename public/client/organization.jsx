c9a66f17a2319ea55a3765cae88f0e76b837f75a
marioyeung <maryeungth@gmail.com>
2016-06-24 12:23:46 -0700

    Insert two react components into Dashboard

---
+++ public/client/organization.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class Organization extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <div> Organization is coming soon </div>
+    )
+  }
+}
+
+export default Organization;
