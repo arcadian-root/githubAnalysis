a3059b964b181eba87a858a1c9e11f547539cc39
Michael Daly <mjdaly95@gmail.com>
2016-06-27 10:37:44 -0700

    Refactored endpoints

--- server/database/database.js
+++ server/database/database.js
@@ -1,4 +1,5 @@
 var url = require('url');
+var querystring = require('querystring');
 
 var neo4j = require('neo4j-driver').v1;
 var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "neo4j1"));
@@ -7,10 +8,19 @@ var session = driver.session();
 
 module.exports = {
   getRepo: function (req, res) {
-    session.run(
-      'MATCH (u:User)-[:CONTRIBUTED_TO]-(n:Repo { name: "' + 
-        req.params.name + '" }) RETURN n, u')
+    let parsed = url.parse(req.url);
+    let query = querystring.parse(parsed.query);
+    let q;
 
+    if (!!query.getUsers === true) {
+      q = 'MATCH (u:User)-[:CONTRIBUTED_TO]->(n:Repo { name: "' + 
+        req.params.name + '" }) RETURN u, n';
+    } else {
+      q = 'MATCH (n:Repo { name: "' + req.params.name + 
+        '" }) RETURN n';
+    }
+
+    session.run(q)
       .then(results => {
         session.close();
         res.end(JSON.stringify(results.records));
@@ -22,10 +32,19 @@ module.exports = {
   },
 
   getUser: function (req, res) {
-    session.run(
-      'MATCH (u:User { login: "' + req.params.login + 
-        '" })-[:CONTRIBUTED_TO]-(n:Repo) RETURN u, n')
-    
+    let parsed = url.parse(req.url);
+    let query = querystring.parse(parsed.query);
+    let q;
+
+    if (!!query.getRepos === true) {
+      q = 'MATCH (u:User { login: "' + req.params.login + 
+        '" })-[:CONTRIBUTED_TO]->(n:Repo) RETURN n, u';
+    } else {
+      q = 'MATCH (u:User { login: "' + req.params.login + 
+        '" }) RETURN u';
+    }
+
+    session.run(q)
       .then(results => {
         session.close();
         res.end(JSON.stringify(results.records));
