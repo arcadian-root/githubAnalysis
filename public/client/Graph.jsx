c528bc1a2e0f6647ef2d482c74164c9fc394f3a6
Andy Tran <adtran117@gmail.com>
2016-07-06 15:03:43 -0700

    fixed merge conflicts

--- public/client/Graph.jsx
+++ public/client/Graph.jsx
@@ -14,7 +14,7 @@ class Graph extends React.Component {
       }).done( (data) => {
         console.log(data)
         if (typeof App !== 'undefined') { 
-		      if (!App.initialized) { App.init(data.username); } // super hacky but who cares 
+		      if (!App.initialized) { App.init(data.username); }
     		} else { 
       		console.error('ERROR: Graph.jsx - App is not defined.\n' + 
         	'Please ensure GitHub-Network-Graph module is installed.'); 
