35a91881342ad77a9a5f0b6dd3eab427f8781a1c
marioyeung <maryeungth@gmail.com>
2016-07-07 16:47:07 -0700

    Add test to test searching function

--- test/spec/clientSpec.js
+++ test/spec/clientSpec.js
@@ -2,13 +2,16 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import React from 'react';
 import { createStore } from 'redux';
+import { ControlLabel } from 'react-bootstrap';
 import { mount, render, shallow } from 'enzyme';
 
 import Main from './../../public/client/main';
 import NavBar from './../../public/client/NavBar';
-import GraphView from './../../public/client/GraphView';
+import GraphContainer from './../../public/client/GraphContainer';
+import Graph from './../../public/client/Graph';
 import Filters from './../../public/client/Filters';
 import SearchBar from './../../public/client/SearchBar';
+
 
 describe('Main', () => {
   const wrapper = shallow(<Main />);
@@ -20,20 +23,21 @@ describe('Main', () => {
   it('should render <NavBar /> component', () => {
     expect(wrapper.find(NavBar)).to.have.length(1);
   });
-})
+});
 
 
-describe('GraphView', () => {
-  const wrapper = shallow(<GraphView />);
+describe('GraphContainer', () => {
+  const wrapper = shallow(<GraphContainer />);
 
   it('should be a stateful class component', () => {
-    expect(React.Component.isPrototypeOf(GraphView)).to.be.true;
+    expect(React.Component.isPrototypeOf(GraphContainer)).to.be.true;
   });
 
-  xit('should render <Filters /> component', () => {
-    expect(wrapper.find(Filters)).to.have.length(1);
-  });
-})
+  it('should render <Graph /> component', () => {
+    expect(wrapper.find(Graph)).to.have.length(1);
+  })
+});
+
 
 describe('Filters', () => {
   const wrapper = shallow(<Filters />);
@@ -41,15 +45,11 @@ describe('Filters', () => {
   it('should be a stateful class component', () => {
     expect(React.Component.isPrototypeOf(Filters)).to.be.true;
   });
+});
 
-})
 
 describe('SearchBar', () => {
   const wrapper = shallow(<SearchBar />);
-
-  xit('should have updateDropDown method', () => {
-
-  });
 
   it('should invoke updateDropDown when \'Repos\' MenuItem is selected', () => {
     const onSelect = sinon.spy();
@@ -61,5 +61,20 @@ describe('SearchBar', () => {
     const onSelect = sinon.spy();
     wrapper.find('MenuItem #usersMenu').simulate('select');
     expect(onSelect.calledOnce).to.equal.true;
+  });
+
+  it('should invoke handleValidInput when user searches users or repos', () => {
+    const onKeyPress = sinon.spy();
+    wrapper.find('FormControl').simulate('keypress', { which: 'abcd' });
+    expect(onKeyPress.calledOnce).to.equal.true;
+  });
+
+  it('should display error message when searching is invalid', () => {
+    wrapper.setState({validationState: 'error', errorType: 'badQuery', dropDownVal: 'Repos'});
+    expect(wrapper.find(ControlLabel)).to.have.length(1);
+    wrapper.setState({validationState: '', errorType: '',});
+    expect(wrapper.find(ControlLabel)).to.have.length(0);
+    wrapper.setState({validationState: 'error', errorType: 'badQuery', dropDownVal: 'Users'});
+    expect(wrapper.find(ControlLabel)).to.have.length(1);
   });
 })
